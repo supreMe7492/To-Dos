e410963f7e1e5b49faa64cbd78a734fe48aabfc7
Supreme Raut <supremeraut94@gmail.com>
2024-10-05 11:30:37 +0545

    added class for tasks

---
+++ src/addTasks.js
@@ -0,0 +1,28 @@
+import { removeForm } from "./taskform";
+
+let tasksArr = [];
+class tasks {
+    constructor(task,date,description,importance){
+        this.task = task;
+        this.date = date;
+        this.description = description;
+        this.importance = importance;
+    }
+}
+
+function addTask(){
+    let task = document.querySelector("#task").value;
+    let date = document.querySelector("#date").value;
+    let description = document.querySelector("#description").value;
+    let importance = document.querySelector("#importance").value;
+    let todo = new tasks(task,date,description,importance);
+   
+    if(!todo.task==''){
+    removeForm();
+    tasksArr.push(todo);
+
+}
+    console.log(tasksArr);
+}
+
+export {addTask};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 import {effect} from "./menueffect.js";
 import { showForm,removeForm } from "./taskform.js";
+import { addTask } from "./addTasks.js";
 
 effect();
 
 let addTaskBtn = document.querySelector(".addTasks");
 let removeIcon = document.querySelector(".removeIcon");
+let addBtn = document.querySelector(".addBtn");
 addTaskBtn.addEventListener("click",showForm);
 removeIcon.addEventListener("click",removeForm);
+addBtn.addEventListener("click",addTask);
